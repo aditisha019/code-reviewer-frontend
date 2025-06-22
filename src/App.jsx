c2f84e9f97c39bd229fabd14df527a3f92b39b4d
Aditi Hotti <155968055+aditisha019@users.noreply.github.com>
2025-06-23 02:07:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import "prismjs/themes/prism-tomorrow.css";
 import Editor from "react-simple-code-editor";
 import prism from "prismjs";
-import "prismjs/components/prism-javascript"; // important for syntax highlighting
+import "prismjs/components/prism-javascript"; 
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
